865b2af009963523c0af9046e8a0b3541dd38d23
diem <levietha170205@gmail.com>
2024-10-15 16:25:03 +0700

    code Linked list

---
+++ linkedList.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct  Node
+{
+    float data;
+    struct Node *next;
+};
+typedef struct Node * node;
+//hàm nhập danh sách đầu
+node nhapDS_First(node &head,int x){
+    node newNode;
+    do{
+        printf("nhap 1 so nguyen(0:dung)");
+        scanf("%d",&x);
+        if (x!=0){   
+            newNode = (node)malloc(sizeof(Node));
+            newNode->data=x;
+            newNode->next=head;
+            head=newNode;
+        }
+    } while (x!=0);
+    return head;
+} 
+//in ra dslk đã nhập
+void printfList(node head){
+    while(head!=NULL){
+        printf("%d",head->data);
+        head=head->next;
+    }
+}
+// cấp phát động một node mới với dữ liệu số nguyên x
+node makeNode(int x){
+    node temp=new Node();
+    temp->data=x;
+    temp->next=NULL;
+    return temp;
+}
+// nhập danh sách liên kết
+int main(){
+    int x, index;
+    node head= NULL;    
+    nhapDS_First(head,x);
+    printf("Danh sach da nhap la:");
+    printfList(head);
+
+
+}
+
