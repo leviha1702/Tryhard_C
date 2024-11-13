31b9bd623426be1414023c2ad24502dc8ad367e9
diem <levietha170205@gmail.com>
2024-11-13 17:14:06 +0700

    Lap3_1

--- test.cpp
+++ test.cpp
@@ -1,8 +1,33 @@
 #include<stdio.h>
-int main(){
-    int n;
-    printf("nhap so n:");
-    scanf("%d",&n);
-    printf("hi everybody");
-    return 0;
+#include<stdlib.h>
+#define MAX 50
+void readFile(int a[],int &n){
+	FILE*f=fopen("Lap3_1.inp","r");
+	if(f==NULL){
+		printf("khong the truy cap file!\n");
+		return;
+	}
+	printf("so luong phan tu trong mang :\n");
+	fscanf(f,"%d",&n);
+	printf("cac phan tu trong mang:\n");
+	for (int i = 0; i < n; i++){
+		fscanf(f,"%d",&a[i]);
+	}
+	fclose(f);
 }
+//số lớn nhất trong mảng vòng lặp
+int findNumMax(int a[],int n){
+	int max=a[0];
+	for (int i = 0; i < n; i++){
+		if(a[i]>max){
+			max=a[i];
+		}
+	}
+	return max;
+}
+int main(){
+	int a[MAX],n;
+	readFile(a,n);
+	
+}
+
