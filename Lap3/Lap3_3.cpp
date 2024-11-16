b8d151a7fd6c641450f5e51951032883eaa09ee9
diem <levietha170205@gmail.com>
2024-11-16 19:20:40 +0700

    code back track

--- Lap3/Lap3_3.cpp
+++ Lap3/Lap3_3.cpp
@@ -1,14 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX 50
+#define MAX 30
 void docFile(int a[],int &n,int &s){
     FILE *f=fopen("ATM.inp","r");
     if(f==NULL){
         printf("khong the mo file!");
         return;
     }
-    fscanf(f,"%d",&n);
-    fscanf(f,"%d",&s);
+    fscanf(f,"%d %d",&n,&s);
     for (int i = 0; i < n; i++)
     {
         fscanf(f,"%d",&a[i]);
@@ -17,15 +16,37 @@ void docFile(int a[],int &n,int &s){
 }
 void xuatFile(int a[],int n,int s){
     printf("\nso to tien:%d",n);
-    printf("\ntong so tien:%d");
+    printf("\ntong so tien:%d",s);
     printf("\ngia tri cac to tien:\n");
     for (int i = 0; i < n; i++)
     {
        printf("%d ",a[i]);
     }
 }
+int n,s,a[MAX],solution[MAX];
+int found = 0 ;
+FILE *g;
+void backtrack(int i,int current_sum){
+    if(current_sum==s){
+        found=1;
+        for(int j =0; j<i;j++){
+            if(solution[j]>0){
+                fprintf(g,"%d",solution[j]);
+            }
+        }
+        fprintf(g,"\n");
+        return;
+    }
+    if(current_sum>s||i >=n){
+        return;
+    }
+    for(int j=0;j<=s/a[i];j++){
+        solution[i]=j*a[i];
+        backtrack(i+1,current_sum+solution[i]);
+    }
+    solution[i]=0;
+}
 int main(){
-    int a[MAX],n,s;
     docFile(a,n,s);
     xuatFile(a,n,s);
 }
